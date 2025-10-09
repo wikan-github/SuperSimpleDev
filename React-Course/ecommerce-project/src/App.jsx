136df82adb3f540e1e40f27d2419459b9300cfcf
Wayan Kastawan <wikansolution@gmail.com>
2025-10-09 19:44:21 +0800

    update links to match the router

--- React-Course/ecommerce-project/src/App.jsx
+++ React-Course/ecommerce-project/src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css';
+import { CheckOut } from './pages/CheckOutPage';
 import { HomePage } from './pages/HomePage';
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router"; 
+
+// import '../styles/shared/general.css';
+import './styles/shared/general.css';
 
 function App() {
 
    return (
-    <BrowserRouter>
+   
     <Routes>
       <Route index  element={<HomePage />} />
-      <Route path='check-out' element={<div>Test Checkout Page</div>}/>
+      <Route path='checkout' element={<CheckOut/>}/>
     </Routes>
-  </BrowserRouter>
+
   ); 
 
    /*  return (
